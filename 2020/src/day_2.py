0d0910157abbe60891a1daedbe2e331dfe45e13b
Eric Kerstens <ekerstens@expediagroup.com>
2020-12-03 11:55:07 -0800

    Day2 Submission

---
+++ 2020/src/day_2.py
@@ -0,0 +1,51 @@
+from typing import List, NamedTuple
+from copy import copy
+
+
+class PasswordPolicy(NamedTuple):
+    min_: int
+    max_: int
+    letter: str
+    password: str
+
+
+def format_line(line):
+    line = line.strip()
+    policy = PasswordPolicy(
+        min_=int(line[0 : line.find("-")]),
+        max_=int(line[line.find("-") + 1 : line.find(" ")]),
+        letter=line[line.find(" ") + 1 : line.find(":")],
+        password=line[line.find(":") + 2 :],
+    )
+    return policy
+
+
+def parse_input(input_file):
+    with open(input_file) as input_data:
+        return [format_line(line) for line in input_data]
+
+
+def problem_1(input_list: List[int]):
+    def password_okay(policy: PasswordPolicy):
+        occurances = policy.password.count(policy.letter)
+        return policy.min_ <= occurances <= policy.max_
+
+    return sum([password_okay(policy) for policy in input_list])
+
+
+def problem_2(input_list: List[int]):
+    def password_okay(policy: PasswordPolicy):
+        return (policy.password[policy.min_ - 1] == policy.letter) ^ (
+            policy.password[policy.max_ - 1] == policy.letter
+        )
+
+    return sum([password_okay(policy) for policy in input_list])
+
+
+if __name__ == "__main__":
+    data = parse_input("resources/day_2_input.txt")
+    result_1 = problem_1(data)
+    print(f"Result 1: {result_1}")
+
+    result_2 = problem_2(data)
+    print(f"Result 2: {result_2}")
