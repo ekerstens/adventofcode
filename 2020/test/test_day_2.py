309c05f42a5fb8f052b8be21ebb127c6d640dcb6
ekerstens <49325583+ekerstens@users.noreply.github.com>
2020-12-04 09:18:57 -0800

    Day2 (#5)
    * Combine logic into abstract AdventDay
    
    * 'Refactored by Sourcery' (#4)
    
    Co-authored-by: Sourcery AI <>
    
    * Fix test cases
    
    Co-authored-by: Eric Kerstens <ekerstens@expediagroup.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- 2020/test/test_day_2.py
+++ 2020/test/test_day_2.py
@@ -1,16 +1,15 @@
-from src.day_2 import problem_1, problem_2, format_line
+from src.day_2 import Day2
 
-test_input = [1721, 979, 366, 299, 675, 1456]
-
+day2 = Day2()
 test_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
-formatted_input = [format_line(line) for line in test_input]
+formatted_input = day2.parse_input(test_input)
 
 
 def test_problem_1():
-    result = problem_1(formatted_input)
+    result = day2.problem_1(formatted_input)
     assert result == 2
 
 
 def test_problem_2():
-    result = problem_2(formatted_input)
+    result = day2.problem_2(formatted_input)
     assert result == 1
